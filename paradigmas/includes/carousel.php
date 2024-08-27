39483d7a2b1dd59fae10f9afbab39b4dc2751810
Manuel Zielinski <107320815+manuzielinski@users.noreply.github.com>
2024-08-27 17:05:46 -0300

    iuh897uyh
    8ug8790yoihjiklñuiolku

---
+++ paradigmas/includes/carousel.php
@@ -0,0 +1,17 @@
+<div class="slideshow-container">
+    <div class="mySlides fade">
+        <img src="assets/images/carousel_img1.png" style="width: 100%">
+    </div>
+    <div class="mySlides fade">
+        <img src="assets/images/carousel_img2.png" style="width: 100%">
+    </div>
+
+    <a class="prev" onclick="plusSlides(-1)">&#10094</a>
+    <a class="next" onclick="plusSlides(1)">&#10095</a>
+</div>
+<br>
+<div style="text-align: center">
+    <span class="dot" onclick="currentSlides(1)"></span>
+    <span class="dot" onclick="currentSlides(2)"></span>
+</div>
+<script src="carousel.js"></script>
