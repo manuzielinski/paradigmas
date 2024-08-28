711a96ae9b7de6a533be8b88af0de92610b9cb2d
Manuel Zielinski <107320815+manuzielinski@users.noreply.github.com>
2024-08-28 12:25:36 -0300

    Implementaciones
    Box-shadow en carousel.
    Reescalado de imágenes.
    Css boton Comprar en Cards.

---
+++ index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Smoke Society</title>
+    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/carousel.css">
+    <link rel="stylesheet" href="assets/css/cards.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+<body>
+
+    <?php include 'includes/header2.php'; ?>
+
+    <?php include 'includes/carousel.php'; ?>
+
+    <div class="container">
+        <div class="card">
+            <div class="product-image">
+                <img src="assets/images/Nikbar4000.jpeg" alt="Nikbar 4000">
+            </div>
+            <div class="product-info">
+                <h3>Nikbar 4000</h3>
+                <h4>$19000 ARS</h4>
+            </div>
+            <div class="btn">
+                <a href="pages/producto.php?product=nikbar4000" class="button-link">Comprar</a>
+            </div>
+        </div>
+        <div class="card">
+            <div class="product-image">
+                <img src="assets/images/Nikbar1500.jpeg" alt="Nikbar 1500">
+            </div>
+            <div class="product-info">
+                <h3>Nikbar 1500</h3>
+                <h4>$9000 ARS</h4>
+            </div>
+            <div class="btn">
+                <a href="pages/producto.php?product=nikbar1500" class="button-link">Comprar</a>
+            </div>
+        </div>
+        <div class="card">
+            <div class="product-image">
+                <img src="assets/images/Nikbar2500.jpeg" alt="Nikbar 2500">
+            </div>
+            <div class="product-info">
+                <h3>Nikbar 2500</h3>
+                <h4>$14000 ARS</h4>
+            </div>
+            <div class="btn">
+                <a href="pages/producto.php?product=nikbar2500" class="button-link">Comprar</a>
+            </div>
+        </div>
+        <div class="card">
+            <div class="product-image">
+                <img src="assets/images/Nikbar6000.jpeg" alt="Nikbar 6000">
+            </div>
+            <div class="product-info">
+                <h3>Nikbar 6000</h3>
+                <h4>$25000 ARS</h4>
+            </div>
+            <div class="btn">
+                <a href="pages/producto.php?product=nikbar6000" class="button-link">Comprar</a>
+            </div>
+        </div>
+        <div class="card">
+            <div class="product-image">
+                <img src="assets/images/Geekbar5000.jpeg" alt="Geekbar 5000">
+            </div>
+            <div class="product-info">
+                <h3>Geekbar 5000</h3>
+                <h4>$20000 ARS</h4>
+            </div>
+            <div class="btn">
+                <a href="pages/producto.php?product=geekbar5000" class="button-link">Comprar</a>
+            </div>
+        </div>
+        <div class="card">
+            <div class="product-image">
+                <img src="assets/images/MammaPro1000.jpeg" alt="Mamma Pro 1000">
+            </div>
+            <div class="product-info">
+                <h3>Mamma Pro 1000</h3>
+                <h4>$5500 ARS</h4>
+            </div>
+            <div class="btn">
+                <a href="pages/producto.php?product=mamma_pro_1000" class="button-link">Comprar</a>
+            </div>
+        </div>
+        <div class="card">
+            <div class="product-image">
+                <img src="assets/images/MaskKing8000.jpeg" alt="Mask King 8000">
+            </div>
+            <div class="product-info">
+                <h3>Mask King 8000</h3>
+                <h4>$27000 ARS</h4>
+            </div>
+            <div class="btn">
+                <a href="pages/producto.php?product=mask_king_8000" class="button-link">Comprar</a>
+            </div>
+        </div>
+        <div class="card">
+            <div class="product-image">
+                <img src="assets/images/Nikbar8000.jpeg" alt="Nikbar 8000">
+            </div>
+            <div class="product-info">
+                <h3>Nikbar 8000</h3>
+                <h4>$16000 ARS</h4>
+            </div>
+            <div class="btn">
+                <a href="pages/producto.php?product=nikbar8000" class="button-link">Comprar</a>
+            </div>
+        </div>
+        <div class="card">
+            <div class="product-image">
+                <img src="assets/images/DozeUp600.jpeg" alt="DozeUp 600">
+            </div>
+            <div class="product-info">
+                <h3>DozeUp 600</h3>
+                <h4>$7000 ARS</h4>
+            </div>
+            <div class="btn">
+                <a href="pages/producto.php?product=dozeup_600" class="button-link">Comprar</a>
+            </div>
+        </div>
+    </div>
+
+    <?php include 'includes/footer.php'; ?>
+    <script src="scripts/carousel.js"></script>
+</body>
+</html>
