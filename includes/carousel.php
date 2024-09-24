6fb7fd966653dbe76f0f13cf458b6d38e3a81e4f
Manuel Zielinski <107320815+manuzielinski@users.noreply.github.com>
2024-09-24 14:43:33 -0300

    asd

--- includes/carousel.php
+++ includes/carousel.php
@@ -5,6 +5,7 @@
     <div class="mySlides fade">
         <img src="assets/images/carousel_imgV4.png" style="width: 100%">
     </div>
+    
 
     <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
     <a class="next" onclick="plusSlides(1)">&#10095;</a>
