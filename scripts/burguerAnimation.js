9c2bf5935f2059f24598dd9496abeffe44d8de45
Manuel Zielinski <107320815+manuzielinski@users.noreply.github.com>
2024-09-25 07:22:16 -0300

    fix

--- scripts/burguerAnimation.js
+++ scripts/burguerAnimation.js
@@ -1,6 +1,8 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const navContainer = document.querySelector('.nav-container');
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navContainer = document.querySelector('.nav-container');
 
-menuToggle.addEventListener('click', () => {
-    navContainer.classList.toggle('active');
+    menuToggle.addEventListener('click', function() {
+        navContainer.classList.toggle('active'); // Asegúrate de que .active esté definido en tu CSS
+    });
 });
