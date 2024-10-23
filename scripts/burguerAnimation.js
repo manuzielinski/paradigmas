511bfdd29f14cd9c6f68385fab5b361c35bc1796
Manuel Zielinski <107320815+manuzielinski@users.noreply.github.com>
2024-10-23 04:37:46 -0300

    Delete scripts directory

--- scripts/burguerAnimation.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navContainer = document.querySelector('.nav-container');
-    menuToggle.addEventListener('click', function() {
-        navContainer.classList.toggle('active');
-    });
-});
